52223857d8b8c2b92f963287a09d34f292fb6e76
Koushik Modak <112518413+KoushikRajaModak@users.noreply.github.com>
2023-08-25 16:57:16 +0530

    add

---
+++ JS/JS DOM/app.js
@@ -0,0 +1,8 @@
+let getvalue = document.querySelector(".Rowselect");
+let getvalue1 = document.querySelector(".ButtonNextPage");
+getvalue1.addEventListener("click", function () {
+  let sevalue = getvalue.value;
+  let ca;
+  console.log(ca[0] == null);
+  alert(sevalue);
+});
